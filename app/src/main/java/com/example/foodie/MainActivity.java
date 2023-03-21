213d58c3672f13fa7466476cfe9b26b436a01422
Lokanatham-Latesh <lokanathamlatesh2003@gmail.com>
2023-03-21 22:12:11 +0530

    Third commit

--- app/src/main/java/com/example/foodie/MainActivity.java
+++ app/src/main/java/com/example/foodie/MainActivity.java
@@ -16,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
         Button bt1 = findViewById(R.id.btn);
         Button bt2 = findViewById(R.id.btn2);
         Button bt3 = findViewById(R.id.btn3);
+        Button bt4 = findViewById(R.id.btn4);
+        Button bt5 = findViewById(R.id.btn5);
+        Button bt6 = findViewById(R.id.btn6);
+
 
         Button  crt1 = findViewById(R.id.cart);
         Button crt2 = findViewById(R.id.cart2);
         Button crt3 = findViewById(R.id.cart3);
+        Button crt4 = findViewById(R.id.cart4);
+        Button crt5 = findViewById(R.id.cart5);
+        Button crt6 = findViewById(R.id.cart6);
+
      bt1.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
@@ -56,6 +64,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         crt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+
+        crt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+        crt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),cart.class);
+                startActivity(myintent);
+            }
+        });
+        crt6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myintent = new Intent(getApplicationContext(),cart.class);
